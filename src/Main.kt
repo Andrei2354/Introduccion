e4e3281d18974cbd5346fb59bd200cb924eadd67
Andrei2354 <andreiaimar1362@gmail.com>
2024-11-06 11:52:41 +0000

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var nombre: String = "Andrei" // String
+    val edad: Int = 20 // Int
+
+    val myNum = 5             // Int
+    val myDoubleNum = 5.99    // Double
+    val myLetter = 'D'        // Char
+    val myBoolean = true      // Boolean
+    val myText = "Hola"      // String
+
+    println(nombre)
+    println(edad)
+}
